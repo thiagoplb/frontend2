7ecb57bac4dd131498553f3e303515123395f619
thiagoplb <thi.plb@gmail.com>
2025-02-13 20:47:11 -0300

    Add files via upload

--- exercicioglaucio.py
+++ exercicioglaucio.py
@@ -31,6 +31,6 @@ for i in lista:
         somaaltura = i.altura + somaaltura
         qthomens = qthomens +1 
         
-print(f"\nA maior altura é de {max(listaaltura)}")
+print(f"\nA maior altura é de {max(listaaltura)} e a menor é de {min(listaaltura)}")
 print(f"A média da altura das pessoas masculinas é de {somaaltura/qthomens}")
 print(f"O numero de pessoas do gênero femínino é de: {PARTICIPANTES-qthomens}\n")
